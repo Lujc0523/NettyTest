9d37f20e9fa0c477beba995c52bdf4b90b680889
Lujc <Lujc0523@outlook.com>
2018-12-18 15:37:47 +0800

    Initial commit

---
+++ NettyEntry/src/main/java/com/lujc/DiscardServerHandler.java
@@ -0,0 +1,28 @@
+package com.lujc;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * DiscardServerHandler class
+ *
+ * @author lujc
+ * @version 1.0
+ * @date 2018/12/13 10:07
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        //super.channelRead(ctx, msg);
+        ((ByteBuf)msg).release();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        //super.exceptionCaught(ctx, cause);
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
